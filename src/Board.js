9cb797d07843363cee35e307f5600d41296ce1f9
Fedor Božík <bzk.333@gmail.com>
2018-06-12 13:25:46 +0200

    name changes and other minor code fixes

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import Square from './Square';
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 
 class Board extends React.Component {
     constructor(props) {
@@ -17,7 +36,6 @@ class Board extends React.Component {
             return;
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
-        squares[i] = 'X';
         this.setState({
             squares: squares,
             xIsNext: !this.state.xIsNext,
@@ -64,26 +82,5 @@ class Board extends React.Component {
         );
     }
 }
-
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
-}
-
 
 export default Board;
